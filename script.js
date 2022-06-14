18fa62a232c054eded3ae7654874cfed62df0205
moussa2811 <moussa.md14@gmail.com>
2022-06-14 13:02:36 +0000

    edit computerPlay function

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+// Rock Paper Scissor Game
+
+//generate computer choice
 let computerPlay = () =>{
-    
-    return computerSelection;
+    /*
+        { random number between 0 and 1 } 
+            * 2 to get between 0 and 2
+        and then floor to get the lower or equal int
+    */
+    let randomNumber = Math.floor( ( Math.random()  *2 ) );
+    switch (randomNumber){
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissor';
+    }
 };
 
+// ask user choice and return its value;
+let userPlay = () => prompt('Enter rock, paper or scissor : ');
+
 //play one round game and return 0 for equality, 1 for computer win and 2 for user win
-let gameRound = (computerSelection, playerSelection) =>{
+let playRound = (computerSelection, playerSelection) =>{
     playerSelection = playerSelection.toLowerCase();
 
     // equality
@@ -41,20 +59,39 @@ let gameRound = (computerSelection, playerSelection) =>{
     
 };
 
+// 5 Game Rounds function
 let game =  () =>{
+
+    alert("Let's Go :-)");
+    //initialise wins
     let playerWin = 0, computerWin = 0, equality = 0 ; win ;
     for (let i = 0; i < 5; i++) {
+
+        //initialise winner of the new round
         win = 0;
-        win = gameRound();
-        if (win == 1) {
+
+        //get computer and user selections
+        let computerSelection = computerPlay ();
+        let userSelection = userPlay ();
+
+        //play the round and get the winner
+        win = playRound(computerSelection, userSelection);
+        if (win == 1) { //computer win
             alert(`You lose ${computerSelection} beats ${playerSelection}`);
             computerWin++;
-        } else if (win == 2){
+        } else if (win == 2){ //user win
             alert(`You win ${playerSelection} beats ${computerSelection}`);
             playerWin++;
-        } else {
+        } else { //equality
             alert(`No winner ${playerSelection} equals ${computerSelection}`);
             equality++;
         }
+    }
+    
+    //alert results and the winner
+    if (playerWin > computerWin) {
+        alert(`You won, ${playerWin} > ${computerWin}, ${equality} nul(s).`)
+    } else {
+        alert(`You lose, ${playerWin} < ${computerWin}, ${equality} nul(s).`)
     }
 }
